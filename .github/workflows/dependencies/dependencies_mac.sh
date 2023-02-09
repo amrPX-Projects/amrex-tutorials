2a917fd185b18658936fc3c5d183976419e60fc9
Nuno Miguel Nobre <nuno.nobre@stfc.ac.uk>
2023-02-09 16:06:30 +0000

    Try old cmake

--- .github/workflows/dependencies/dependencies_mac.sh
+++ .github/workflows/dependencies/dependencies_mac.sh
@@ -7,8 +7,11 @@
 
 set -eu -o pipefail
 
+wget https://raw.githubusercontent.com/Homebrew/homebrew-core/777f4d4ebb00ca2b38b00ef32ee7f30704b61f35/Formula/cmake.rb
 brew update
+brew unlink cmake
 brew reinstall gcc || true
 brew install libomp || true
 brew install open-mpi || true
 brew install ccache || true
+brew install -s ./cmake.rb || true
